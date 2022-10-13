9367fd1720f2f5fa968916c4110207f92e2e92e4
Aqirito <aqil.rahmat99@gmail.com>
2022-10-13 11:32:48 +0800

    UPDATE: design template for object detection

--- object_detection/src/app/app.component.ts
+++ object_detection/src/app/app.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'object_detection';
+  video: any;
+  canvas: any;
+  ctx: any;
+  cameras: any = [];
+
+  ngOnInit(): void {
+    this.checkCameraSource();
+
+    this.video = document.getElementById('video') as HTMLVideoElement;
+    this.canvas = document.getElementById('canvas') as HTMLCanvasElement;
+
+    this.ctx = this.canvas.getContext('2d');
+  }
+
+  async checkCameraSource() {
+    this.cameras = await navigator.mediaDevices.enumerateDevices();
+  }
+
+  async cameraSource(event: any) {
+    const selectedCamera = event.target?.value
+
+    const constsr = {
+      video: {
+        width: 1280,
+        height: 960,
+        deviceId: {
+          exact: selectedCamera
+        }
+      },
+      audio: false,
+    }
+    await navigator.mediaDevices.getUserMedia(constsr).then(stream => {
+      this.video.srcObject = stream;
+    });
+  }
 }
